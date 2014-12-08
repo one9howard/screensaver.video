bdb724de36b37df2924a7f75f2779392ac1eb1b9
robwebset@gmail.com <robwebset@users.noreply.github.com>
2014-12-08 17:09:42 +0000

    VideoScreensaver: Initial Version

--- resources/lib/settings.py
+++ resources/lib/settings.py
@@ -55,11 +55,11 @@ def dir_exists(dirpath):
 # Stores Various Settings
 ##############################
 class Settings():
+    # ["Aquarium 3  - [6.6GB] - 1080p", "Aquarium003-1080p.mkv", ""]
     # ["Aquarium 4  - [10GB]  - 1080p", "Aquarium003-1080p.mkv", ""]
     PRESET_VIDEOS = (
         ["Aquarium 1  - [846MB] - 400p", "Aquarium001.mkv", "https://onedrive.live.com/download?resid=80BD16963F5C21B5!127&authkey=!ALex8PkcwV8LVJg"],
         ["Aquarium 2  - [2.7GB] - 720p", "Aquarium002-720p.mkv", "https://onedrive.live.com/download?resid=80BD16963F5C21B5!128&authkey=!APnNfC8YAC20UmE"],
-        ["Aquarium 3  - [6.6GB] - 1080p", "Aquarium003-1080p.mkv", ""],
         ["Fireplace 1 - [965MB] - 720p", "Fireplace001-720p.mkv", "https://onedrive.live.com/download?resid=80BD16963F5C21B5!129&authkey=!AGEezA6TTGU_urM"],
         ["Fireplace 2 - [827MB] - 480p", "Fireplace002.mkv", "https://onedrive.live.com/download?resid=80BD16963F5C21B5!130&authkey=!AOf_ClWmjzqkouQ"],
         ["Fireplace 3 - [2.1GB] - 1080p", "Fireplace03-1080p.mkv", "https://onedrive.live.com/download?resid=80BD16963F5C21B5!131&authkey=!ACZmSAeDTUxdz20"]
